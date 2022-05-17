4ba464964c56b0cf2d0e1c10b0a398c0150bf83a
Debasis Pattanaik <67583843+debasis-pattanaik-au16@users.noreply.github.com>
2022-05-17 11:33:04 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TodoModule } from './todo/todo.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://blog-project:dJqqxTYcDNsGB5WR@surreal-blogs.jlyml.mongodb.net/nestMongo?retryWrites=true&w=majority',
+      '',
     ),
     TodoModule,
   ],
